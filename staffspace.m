353ba3bc345c131df97fb6daa468f8a6f5a7458c
kalleankah <k.eknefelt@gmail.com>
2018-12-03 10:53:31 +0100

    Fixat staff line detector

--- staffspace.m
+++ staffspace.m
@@ -3,21 +3,28 @@ function [HalfNoteHeight, NumStaffSegs, peaks] = staffspace(grayimg)
     BW = grayimg > graythresh(grayimg);
     SE_horline = strel('line', 20, 0);
     BW = imopen(BW, SE_horline);
-    SE_rectum = strel('rectangle', [3,15]);
-    BW2 = imopen(BW, SE_rectum);
+    SE_rect = strel('rectangle', [3,15]);
+    BW2 = imopen(BW, SE_rect);
     BW = BW - BW2;
     
     % Horizontal Projection
     HorizontalSum = sum(BW, 2);
-    HorizontalSumSmooth = smoothdata(HorizontalSum,'movmean', length(HorizontalSum)/12);
-    [pks] = findpeaks(HorizontalSumSmooth);
-    NumStaffSegs = length(pks);
+    HorizontalSumSmooth = smoothdata(HorizontalSum,'movmean', length(HorizontalSum)/10);
+    [pks, ~, w] = findpeaks(HorizontalSumSmooth);
+    
+    % Remove narrow peaks
+    map_width = w >= 0.5*max(w);
+    pks_width_filtered = pks .* map_width;
+    pks_width_filtered = pks_width_filtered(pks_width_filtered ~= 0);
+    
+    % Calculate number of staff lines
+    NumStaffSegs = length(pks_width_filtered);
     NumStaffLines = NumStaffSegs*5;
     
     % Select the number of peaks we have staff lines and store in "peaks"
     [pks, locs] = findpeaks(HorizontalSum);
     pks_sorted = sort(pks, 'descend');
-    map = pks>=pks_sorted(NumStaffLines);
+    map = pks >= pks_sorted(NumStaffLines);
     locs_result = locs .* map;
     peaks = locs_result(locs_result ~= 0);
     

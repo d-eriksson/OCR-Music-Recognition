e4474bcffdd20fa101bdcd1a9ffa59ff9547298e
kalleankah <k.eknefelt@gmail.com>
2018-12-06 13:21:37 +0100

    Tillagt kommentarer

--- recognition.m
+++ recognition.m
@@ -4,27 +4,33 @@ close all
 clear
 grayimg = rgb2gray(im2double(imread('im13s.jpg'))).*(-1)+1;
 
-%% Perform automatic rotation
+% Perform automatic rotation
 grayimg = autorotate(grayimg);
 
-%% Define size of staff segments (find absolute size measurement)
+%% All major identification steps
+% Find stafflines and define size of staff segments (absolute measurement)
 [HalfNoteHeight, NumStaffSegs, peaks] = staffspace(grayimg);
 
+% Find the positions of all notes
 centroids = FindNotePositions(grayimg,HalfNoteHeight);
 
+% Identify all notes that are grouped together
 [centroids, STEMS] = NoteGroupIdentify(grayimg, centroids, HalfNoteHeight);
+
+% Identify all notes that are not grouped
 centroids = FindSingleNotes(centroids, STEMS, HalfNoteHeight, grayimg);
 
-
+% Plot the identified notes over the original (rotated) image
 imshow(grayimg)
 hold on
     plot(centroids(centroids(:,3)==4,1),centroids(centroids(:,3)==4,2),'xm');
     plot(centroids(centroids(:,3)==8,1),centroids(centroids(:,3)==8,2),'+b');
     plot(centroids(centroids(:,3)==16,1),centroids(centroids(:,3)==16,2),'*r');
 hold off
-%% Find stafflines and their location
 
+%% Generate a string and play the notes
 [STR,Notething] = generate_string(centroids, HalfNoteHeight, NumStaffSegs, peaks);
+
 if(false)
     PlayMusic(Notething);
 end
